c82e4feb85ec6c18999f3b2d2a5d098d00af1736
sogo <sogonishi@gmail.com>
2025-10-07 14:33:35 -0400

    Initial Commit

---
+++ locomi_v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  locomi_v2
+//
+//  Created by Sogo Nishihara on 2025/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ locomi_v2Tests/locomi_v2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  locomi_v2Tests.swift
+//  locomi_v2Tests
+//
+//  Created by Sogo Nishihara on 2025/10/07.
+//
+
+import Testing
+@testable import locomi_v2
+
+struct locomi_v2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
